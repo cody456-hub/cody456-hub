2254c9ee84cb1f56f2f74c0a75f44f2a1caa630d
cody456-hub <abcxjnc@gmail.com>
2025-02-07 21:40:21 +0800

    update

---
+++  jhm2homework/比較兩個價錢的大小.py
@@ -0,0 +1,15 @@
+first_number = input("Enter your price：")
+second_number = input("Enter another price：")
+numbera = int(first_number)
+numberb = int(second_number)
+
+
+if numbera > numberb :
+    print("The first price is larger than the second one.")
+
+elif numbera < numberb :
+    print("The first price is smaller than the second one.")
+
+elif numbera == numberb :
+    print("The price are the same.")
+
