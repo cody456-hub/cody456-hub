be1a650284072a01043e09f82ff8afbb83ae51ad
cody456-hub <abcxjnc@gmail.com>
2025-02-07 14:06:32 +0000

    update

---  jhm2homework/加法口訣表.py
+++  jhm2homework/加法口訣表.py
@@ -5,7 +5,7 @@ print("------------------------------------------------------------")
 for i in range(1, size + 1):
     for j in range(1, size + 1):
         sum = i + j        
-        if sum_ij < 10:
+        if sum < 10:
             print(f"{i} + {j} = {sum}  ", end=" ")
         else:
             print(f"{i} + {j} = {sum} ", end=" ")
