e2845387674f750027ac577a304d6fa881087356
cody456-hub <abcxjnc@gmail.com>
2025-02-08 09:29:40 +0800

    input

---  jhm2homework/加法口訣表.py
+++  jhm2homework/加法口訣表.py
@@ -1,11 +1,11 @@
-size = int(input("請輸入加法口訣表的大小（小於10的正整數）："))
+size = int(input("Input Addition Table Size smaller 10:"))
 
 print("------------------------------------------------------------")
 
-for i in range(1, size + 1):
-    for j in range(1, size + 1):
+for i in range(1, size + 1 ):
+    for j in range(1, size + 1 ):
         sum = i + j        
-        if sum < 10:
+        if sum < 10 :
             print(f"{i} + {j} = {sum}  ", end=" ")
         else:
             print(f"{i} + {j} = {sum} ", end=" ")
