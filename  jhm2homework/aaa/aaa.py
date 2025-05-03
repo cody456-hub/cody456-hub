2c2285b7cccfdb45d0e2f37e96e0873ea18faa16
cody456-hub <abcxjnc@gmail.com>
2025-05-03 09:42:41 +0000

    update

---  jhm2homework/aaa/aaa.py
+++  jhm2homework/aaa/aaa.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 
 # 設定股票代碼
-ticker = "AAPL"
+ticker = ""
 
 # 設定時間範圍
 start_date = "2020-01-01"
@@ -11,7 +11,8 @@ end_date = "2025-04-25"
 df = yf.download(ticker, start=start_date, end=end_date)
 
 # 存為 CSV 文件
-csv_filename = f"{ticker}_stock_data.csv"
-df.to_csv(csv_filename)
+'SPY_history.xlsx' = f"{ticker}_stock_data.csv"
+df.to_csv('SPY_history.xlsx')
 
-print(f"{ticker} 的數據已保存到 {csv_filename}")
+print(f"{ticker} 的數據已保存到 {'SPY_history.xlsx'}")
+'SPY_history.xlsx'.to_excel('SPY_history.xlsx')
