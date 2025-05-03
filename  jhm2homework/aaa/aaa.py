120e6c50c3467b35b7c2b16aed5e1b8eed88825b
cody456-hub <abcxjnc@gmail.com>
2025-05-03 10:13:58 +0000

    update

---  jhm2homework/aaa/aaa.py
+++  jhm2homework/aaa/aaa.py
@@ -8,6 +8,6 @@ end_date = "2025-04-25"
 
 # 下载数据并保存到单独的 CSV 文件
 for ticker in tickers:
-    df = yf.download(ticker, start=start_date, end=end_date)
+    df = pd.download(ticker, start=start_date, end=end_date)
     df.to_csv(f"{ticker}_stock_data.csv")
     print(f"{ticker} 的数据已保存到 {ticker}_stock_data.csv")
